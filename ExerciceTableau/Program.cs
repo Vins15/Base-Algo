6dd1095d083ff9add68393560be8c95e6015dd7f
Vins15 <34624862+Vins15@users.noreply.github.com>
2018-01-08 17:16:07 +0100

    changement de code

--- ExerciceTableau/Program.cs
+++ ExerciceTableau/Program.cs
@@ -4,23 +4,21 @@ namespace ExerciceTableau
 {
     class Program
     {
+        private static int n;
+
         static void Main(string[] args)
         {
-            int[] tab = {2,8,13,11,6,3,2,1};
-            int elem = 13;
-
-            void AfficherIndice()
-            {
-                for (int i = 0; i < tab.Length; i++)
+            int[] tab = {2,8,13,11,6,3,2,1}; // le tableau initial
+            int elem = tab[0]; //la valeur maximale a stocké
+ 
+                for (int i = 0; i <n; i++)
                 {
                     if (tab[i] > elem)
                     {
                         elem = tab[i];
                     }
                 }
-
-            }
-            AfficherIndice();
+            Console.WriteLine(elem);
         }
     }
 }
