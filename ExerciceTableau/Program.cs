988e1c30d53c25af61ea71b97fd0195abfdf34be
Vins15 <34624862+Vins15@users.noreply.github.com>
2018-01-08 16:57:46 +0100

    ajout du projet Exercice1

---
+++ ExerciceTableau/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExerciceTableau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tab = {2,8,13,11,6,3,2,1};
+            int elem = 13;
+
+            void AfficherIndice()
+            {
+                for (int i = 0; i < tab.Length; i++)
+                {
+                    if (tab[i] > elem)
+                    {
+                        elem = tab[i];
+                    }
+                }
+
+            }
+            AfficherIndice();
+        }
+    }
+}
